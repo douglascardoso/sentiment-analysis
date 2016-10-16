4de65cae3f0e29a37f63e672dd5293813682f49e
douglas <douglas@douglas>
2016-10-16 12:25:23 -0200

    Message

--- src/main/java/br/com/pucpr/tcc/GeneretaTraining.java
+++ src/main/java/br/com/pucpr/tcc/GeneretaTraining.java
@@ -1,6 +1,12 @@
 package br.com.pucpr.tcc;
 
-import br.com.pucpr.tcc.util.Word2VecDataSet;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.models.embeddings.WeightLookupTable;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
@@ -24,227 +30,193 @@ import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import br.com.pucpr.tcc.util.Word2VecDataSet;
 
 /**
  * Created by douglas on 10/13/16.
  */
 public class GeneretaTraining {
 
-    private static final Logger log = LoggerFactory.getLogger(GeneretaTraining.class);
+	private static final Logger log = LoggerFactory.getLogger(GeneretaTraining.class);
 
-    private List<Tweet2> trainLines = new ArrayList<>();
-    private List<Tweet2> testLines = new ArrayList<>();
-    private WeightLookupTable lookuptable;
+	private WeightLookupTable lookuptable;
 
-    public GeneretaTraining() {
+	public GeneretaTraining() {
 
-        lookuptable = Word2VecDataSet.lookupTable("/home/douglas/Desktop/word2vec.txt");
+		lookuptable = Word2VecDataSet.lookupTable("/home/douglas/datasets/word2vec.txt");
 
-    }
+	}
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        GeneretaTraining main = new GeneretaTraining();
-        main.run();
+		GeneretaTraining main = new GeneretaTraining();
+		main.run();
 
-    }
+	}
 
-    public void run() throws IOException {
-        String filePath = "/home/douglas/PUCPR/tcc/sentiment-analysis/src/main/resources/semeval/train.tsv";
-        readFile(filePath);
+	public void run() throws IOException {
+		String filePath = "/home/douglas/sentiment-analysis/src/main/resources/semeval/train.tsv";
+		List<Tweet2> trainLines = readFile(filePath);
 
-        filePath = "/home/douglas/PUCPR/tcc/sentiment-analysis/src/main/resources/semeval/validation.tsv";
-        readTestFile(filePath);
+		filePath = "/home/douglas/sentiment-analysis/src/main/resources/semeval/validation.tsv";
+		List<Tweet2> testLines = readFile(filePath);
 
-        INDArray trainArray = Nd4j.zeros(trainLines.size(), 10000);
-        INDArray trainOutcomes = Nd4j.zeros(trainLines.size(), 2);
+		INDArray trainArray = Nd4j.zeros(trainLines.size(), 10000);
+		INDArray trainOutcomes = Nd4j.zeros(trainLines.size(), 2);
 
-        INDArray testArray = Nd4j.zeros(testLines.size(), 10000);
-        INDArray testOutcomes = Nd4j.zeros(testLines.size(), 2);
+		INDArray testArray = Nd4j.zeros(testLines.size(), 10000);
+		INDArray testOutcomes = Nd4j.zeros(testLines.size(), 2);
 
-        //Train
-        for (int i = 0; i < trainLines.size(); i++) {
-            Tweet2 tweet = trainLines.get(i);
-            trainOutcomes.getRow(i).getColumn(tweet.getLabel()).assign(tweet.getLabel());
-            trainArray.getRow(i).assign(tweet.getFeatures());
-        }
+		// Train
+		for (int i = 0; i < trainLines.size(); i++) {
+			Tweet2 tweet = trainLines.get(i);
+			trainOutcomes.getRow(i).getColumn(tweet.getLabel()).assign(tweet.getLabel());
+			trainArray.getRow(i).assign(tweet.getFeatures());
+		}
 
-        //Validation
-        for (int i = 0; i < testLines.size(); i++) {
-            Tweet2 tweet = testLines.get(i);
-            trainOutcomes.getRow(i).getColumn(tweet.getLabel()).assign(tweet.getLabel());
-            trainArray.getRow(i).assign(tweet.getFeatures());
-        }
+		// Validation
+		for (int i = 0; i < testLines.size(); i++) {
+			Tweet2 tweet = testLines.get(i);
+			trainOutcomes.getRow(i).getColumn(tweet.getLabel()).assign(tweet.getLabel());
+			trainArray.getRow(i).assign(tweet.getFeatures());
+		}
 
-        int nChannels = 1;
-        int outputNum = 2;
-        int batchSize = 64;
-        int nEpochs = 10;
-        int iterations = 1;
-        int seed = 123;
+		System.gc();
 
-        DataSet train = new DataSet(trainArray, trainOutcomes);
-        DataSet test = new DataSet(testArray, testOutcomes);
+		int nChannels = 1;
+		int outputNum = 2;
+		int batchSize = 64;
+		int nEpochs = 10;
+		int iterations = 1;
+		int seed = 123;
 
-        DataSetIterator trainIterator = new SamplingDataSetIterator(train, batchSize, trainLines.size());
-        DataSetIterator testIterator = new SamplingDataSetIterator(test, batchSize, testLines.size());
+		DataSet train = new DataSet(trainArray, trainOutcomes);
+		DataSet test = new DataSet(testArray, testOutcomes);
 
-        MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder()
-                .seed(seed)
-                .iterations(iterations)
-                .regularization(true).l2(0.0005)
-                .learningRate(0.01)//.biasLearningRate(0.02)
-                //.learningRateDecayPolicy(LearningRatePolicy.Inverse).lrPolicyDecayRate(0.001).lrPolicyPower(0.75)
-                .weightInit(WeightInit.XAVIER)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .updater(Updater.NESTEROVS).momentum(0.9)
-                .list()
-                .layer(0, new ConvolutionLayer.Builder(5, 5)
-                        //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters to be applied
-                        .nIn(nChannels)
-                        .stride(1, 1)
-                        .nOut(20)
-                        .activation("identity")
-                        .build())
-                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                        .kernelSize(2, 2)
-                        .stride(2, 2)
-                        .build())
-                .layer(2, new ConvolutionLayer.Builder(5, 5)
-                        //Note that nIn need not be specified in later layers
-                        .stride(1, 1)
-                        .nOut(50)
-                        .activation("identity")
-                        .build())
-                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                        .kernelSize(2, 2)
-                        .stride(2, 2)
-                        .build())
-                .layer(4, new DenseLayer.Builder().activation("relu")
-                        .nOut(500).build())
-                .layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                        .nOut(outputNum)
-                        .activation("softmax")
-                        .build())
-                .backprop(true).pretrain(false);
-        // The builder needs the dimensions of the image along with the number of channels. these are 28x28 images in one channel
-        new ConvolutionLayerSetup(builder, 100, 100, 1);
+		DataSetIterator trainIterator = new SamplingDataSetIterator(train, batchSize, trainLines.size());
+		DataSetIterator testIterator = new SamplingDataSetIterator(test, batchSize, testLines.size());
 
-        MultiLayerConfiguration conf = builder.build();
-        MultiLayerNetwork model = new MultiLayerNetwork(conf);
-        model.init();
+		MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder().seed(seed).iterations(iterations)
+				.regularization(true).l2(0.0005).learningRate(0.01)// .biasLearningRate(0.02)
+				// .learningRateDecayPolicy(LearningRatePolicy.Inverse).lrPolicyDecayRate(0.001).lrPolicyPower(0.75)
+				.weightInit(WeightInit.XAVIER).optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+				.updater(Updater.NESTEROVS).momentum(0.9).list()
+				.layer(0,
+						new ConvolutionLayer.Builder(5, 5)
+								// nIn and nOut specify depth. nIn here is the
+								// nChannels and nOut is the number of filters
+								// to be applied
+								.nIn(nChannels).stride(1, 1).nOut(20).activation("identity").build())
+				.layer(1,
+						new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX).kernelSize(2, 2).stride(2, 2)
+								.build())
+				.layer(2,
+						new ConvolutionLayer.Builder(5, 5)
+								// Note that nIn need not be specified in later
+								// layers
+								.stride(1, 1).nOut(50).activation("identity").build())
+				.layer(3,
+						new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX).kernelSize(2, 2).stride(2, 2)
+								.build())
+				.layer(4, new DenseLayer.Builder().activation("relu").nOut(500).build())
+				.layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD).nOut(outputNum)
+						.activation("softmax").build())
+				.backprop(true).pretrain(false);
+		// The builder needs the dimensions of the image along with the number
+		// of channels. these are 28x28 images in one channel
+		new ConvolutionLayerSetup(builder, 100, 100, 1);
 
-        log.info("Train model....");
-        model.setListeners(new ScoreIterationListener(1));
-        for (int i = 0; i < nEpochs; i++) {
-            model.fit(trainIterator);
-            log.info("*** Completed epoch {} ***", i);
+		MultiLayerConfiguration conf = builder.build();
+		MultiLayerNetwork model = new MultiLayerNetwork(conf);
+		model.init();
 
-            log.info("Evaluate model....");
-            Evaluation eval = new Evaluation(outputNum);
-            while (testIterator.hasNext()) {
-                DataSet ds = testIterator.next();
-                INDArray output = model.output(ds.getFeatureMatrix(), false);
-                eval.eval(ds.getLabels(), output);
-            }
-            log.info(eval.stats());
-            testIterator.reset();
-        }
-        log.info("****************Example finished********************");
+		log.info("Train model....");
+		model.setListeners(new ScoreIterationListener(1));
+		for (int i = 0; i < nEpochs; i++) {
+			model.fit(trainIterator);
+			log.info("*** Completed epoch {} ***", i);
 
-    }
+			log.info("Evaluate model....");
+			Evaluation eval = new Evaluation(outputNum);
+			while (testIterator.hasNext()) {
+				DataSet ds = testIterator.next();
+				INDArray output = model.output(ds.getFeatureMatrix(), false);
+				eval.eval(ds.getLabels(), output);
+			}
+			log.info(eval.stats());
+			testIterator.reset();
+		}
+		log.info("****************Example finished********************");
 
-    public void readFile(String path) {
-        BufferedReader file = null;
-        try {
-            file = new BufferedReader(new FileReader(path));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-        try {
-            while (file.ready()) {
-                String line = file.readLine();
-                String[] tokens = line.split("\t");
-                Tweet2 tweet = parseTweet(tokens[1]);
-                tweet.setLabel(parseLabel(tokens[0]));
-                trainLines.add(tweet);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	}
 
-    public void readTestFile(String path) {
-        BufferedReader file = null;
-        try {
-            file = new BufferedReader(new FileReader(path));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-        try {
-            while (file.ready()) {
-                String line = file.readLine();
-                String[] tokens = line.split("\t");
-                Tweet2 tweet = parseTweet(tokens[1]);
-                tweet.setLabel(parseLabel(tokens[0]));
-                testLines.add(tweet);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public List<Tweet2> readFile(String path) {
+		List<Tweet2> lines = new ArrayList<>();
+		BufferedReader file = null;
+		try {
+			file = new BufferedReader(new FileReader(path));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		try {
+			while (file.ready()) {
+				String line = file.readLine();
+				String[] tokens = line.split("\t");
+				Tweet2 tweet = parseTweet(tokens[1]);
+				tweet.setLabel(parseLabel(tokens[0]));
+				lines.add(tweet);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return lines;
+	}
 
-    private Tweet2 parseTweet(String line) {
-        Tweet2 tweet = new Tweet2();
+	private Tweet2 parseTweet(String line) {
+		Tweet2 tweet = new Tweet2();
 
-        String[] tokens = line.split(" ");
-        INDArray array = null; // = Nd4j.zeros(10000); //max size
+		String[] tokens = line.split(" ");
+		INDArray array = null; // = Nd4j.zeros(10000); //max size
 
-        int cont = 0;
-        for (String token : tokens) {
-            if (cont >= 100) break;
-            INDArray features = lookuptable.vector(token.toLowerCase());
+		int cont = 0;
+		for (String token : tokens) {
+			if (cont >= 100)
+				break;
+			INDArray features = lookuptable.vector(token.toLowerCase());
 
-            if (features != null) {
-                if (array == null) {
-                    array = features;
-                } else {
-                    array = Nd4j.concat(1, array, features);
-                }
-            } else {
-                if (array == null)
-                    array = Nd4j.zeros(1, 100);
-                else
-                    array = Nd4j.concat(1, array, Nd4j.zeros(1, 100));
-            }
+			if (features != null) {
+				if (array == null) {
+					array = features;
+				} else {
+					array = Nd4j.concat(1, array, features);
+				}
+			} else {
+				if (array == null)
+					array = Nd4j.zeros(1, 100);
+				else
+					array = Nd4j.concat(1, array, Nd4j.zeros(1, 100));
+			}
 
-            cont++;
-        }
+			cont++;
+		}
 
-        if (cont < 100) {
+		if (cont < 100) {
 
-            for (int i = 0; i < 100 - cont; i++) {
+			for (int i = 0; i < 100 - cont; i++) {
 
-                array = Nd4j.concat(1, array, Nd4j.zeros(1, 100));
+				array = Nd4j.concat(1, array, Nd4j.zeros(1, 100));
 
-            }
+			}
 
-        }
+		}
 
-        tweet.setFeatures(array);
-        return tweet;
-    }
+		tweet.setFeatures(array);
+		return tweet;
+	}
 
-    private int parseLabel(String line) {
-        return 0;
-    }
+	private int parseLabel(String line) {
+		return 0;
+	}
 
 }
--- src/main/java/br/com/pucpr/tcc/SentimentAnalysisApp.java
+++ src/main/java/br/com/pucpr/tcc/SentimentAnalysisApp.java
@@ -1,101 +1,115 @@
 package br.com.pucpr.tcc;
 
+import java.io.IOException;
+
 import br.com.pucpr.tcc.util.DataSetUtils;
 import br.com.pucpr.tcc.util.Model;
 import br.com.pucpr.tcc.util.Word2VecDataSet;
-
-import java.io.IOException;
 
 /**
  * Created by douglas on 9/29/16.
  */
 public class SentimentAnalysisApp {
 
-    public static final void main(String[] args) throws IOException {
+	public static final void main(String[] args) throws IOException {
 
-        if (args.length > 0) {
+		if (args.length > 0) {
 
-            for (int i = 0; i < args.length; i++) {
-                switch (args[i]) {
-                    case "-clear-corpus-twitter": {
-                        if (args.length < 5) {
-                            throw new RuntimeException("See usage");
-                        }
-                        long startTime = System.currentTimeMillis();
+			for (int i = 0; i < args.length; i++) {
+				switch (args[i]) {
+				case "-clear-corpus-twitter": {
+					if (args.length < 5) {
+						throw new RuntimeException("See usage");
+					}
+					long startTime = System.currentTimeMillis();
 
-                        String inputFile = ClassLoader.getSystemResource("corpus_twitter/training.csv").getPath();
-                        String outputFile = null;
-                        Integer workers = 10;
-                        for (int j = i + 1; j < (i + 4) && j < args.length; j += 2) { //ignoring the first argument
-                            if ("--i".equals(args[j])) {
-                                inputFile = args[j + 1];
-                            } else if ("--o".equals(args[j])) {
-                                outputFile = args[j + 1];
-                            } else if ("--w".equals(args[j])) {
-                                workers = Integer.parseInt(args[j + 1]);
-                            }
-                        }
-                        if (outputFile == null) {
-                            throw new RuntimeException("See usage");
-                        }
-                        DataSetUtils.cleanCorpusTwitter(inputFile, outputFile, workers);
-                        long endTime = System.currentTimeMillis();
-                        System.out.printf("Cleaning corpus twitter is finished with %d seconds\n", (endTime - startTime) / 1000);
-                    }
-                    break;
-                    case "-word2vec-model": {
-                        if (args.length < 5) {
-                            throw new RuntimeException("See usage");
-                        }
+					String inputFile = ClassLoader.getSystemResource("corpus_twitter/training.csv").getPath();
+					String outputFile = null;
+					Integer workers = 10;
+					for (int j = i + 1; j < (i + 4) && j < args.length; j += 2) { // ignoring
+																					// the
+																					// first
+																					// argument
+						if ("--i".equals(args[j])) {
+							inputFile = args[j + 1];
+						} else if ("--o".equals(args[j])) {
+							outputFile = args[j + 1];
+						} else if ("--w".equals(args[j])) {
+							workers = Integer.parseInt(args[j + 1]);
+						}
+					}
+					if (outputFile == null) {
+						throw new RuntimeException("See usage");
+					}
+					DataSetUtils.cleanCorpusTwitter(inputFile, outputFile, workers);
+					long endTime = System.currentTimeMillis();
+					System.out.printf("Cleaning corpus twitter is finished with %d seconds\n",
+							(endTime - startTime) / 1000);
+				}
+					break;
+				case "-word2vec-model": {
+					if (args.length < 5) {
+						throw new RuntimeException("See usage");
+					}
 
-                        String trainFile = null;
-                        String outputFile = null;
+					String trainFile = null;
+					String outputFile = null;
 
-                        for (int j = i + 1; j < (i + 4) && j < args.length; j += 2) { //ignoring the first argument
-                            if ("--i".equals(args[j])) {
-                                trainFile = args[j + 1];
-                            } else if ("--o".equals(args[j])) {
-                                outputFile = args[j + 1];
-                            }
-                        }
-                        if (trainFile == null || outputFile == null) {
-                            throw new RuntimeException("See usage");
-                        }
+					for (int j = i + 1; j < (i + 4) && j < args.length; j += 2) { // ignoring
+																					// the
+																					// first
+																					// argument
+						if ("--i".equals(args[j])) {
+							trainFile = args[j + 1];
+						} else if ("--o".equals(args[j])) {
+							outputFile = args[j + 1];
+						}
+					}
+					if (trainFile == null || outputFile == null) {
+						throw new RuntimeException("See usage");
+					}
 
-                        Word2VecDataSet word2vec = new Word2VecDataSet(trainFile, outputFile);
-                        word2vec.train();
-                    }
-                    break;
-                    case "-create-train-dataset": {
-                        if (args.length < 7) {
-                            throw new RuntimeException("See usage");
-                        }
+					Word2VecDataSet word2vec = new Word2VecDataSet(trainFile, outputFile);
+					word2vec.train();
+				}
+					break;
+				case "-create-train-dataset": {
+					if (args.length < 7) {
+						throw new RuntimeException("See usage");
+					}
 
-                        String word2vecModel = null;
-                        String outputPath = null;
-                        String datasetPath = null;
+					String word2vecModel = null;
+					String outputPath = null;
+					String datasetPath = null;
 
-                        for (int j = i + 1; j < args.length; j += 2) { //ignoring the first argument
-                            if ("--i".equals(args[j])) {
-                                word2vecModel = args[j + 1];
-                            } else if ("--o".equals(args[j])) {
-                                outputPath = args[j + 1];
-                            } else if ("--d".equals(args[j])) {
-                                datasetPath = args[j + 1];
-                            }
-                        }
-                        if (word2vecModel == null || outputPath == null || datasetPath == null) {
-                            throw new RuntimeException("See usage");
-                        }
+					for (int j = i + 1; j < args.length; j += 2) { // ignoring
+																	// the first
+																	// argument
+						if ("--i".equals(args[j])) {
+							word2vecModel = args[j + 1];
+						} else if ("--o".equals(args[j])) {
+							outputPath = args[j + 1];
+						} else if ("--d".equals(args[j])) {
+							datasetPath = args[j + 1];
+						}
+					}
+					if (word2vecModel == null || outputPath == null || datasetPath == null) {
+						throw new RuntimeException("See usage");
+					}
 
-                        Model model = new Model(word2vecModel, outputPath);
-                        model.generateFile(datasetPath, 100);
-                    }
-                    break;
-                }
-            }
-        }
+					Model model = new Model(word2vecModel, outputPath);
+					model.generateFile(datasetPath, 100);
+				}
+					break;
+				case "-train": {
+					GeneretaTraining main = new GeneretaTraining();
+					main.run();
+				}
+					break;
+				}
+			}
+		}
 
-        return;
-    }
+		return;
+	}
 }
